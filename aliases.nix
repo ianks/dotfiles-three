a95e66cb9fbb21b9148663806b75b89a6bfe08a9
Ian Ker-Seymer <hello@ianks.com>
2024-01-26 22:22:28 -0500

    Fix `gpsh` alias

--- aliases.nix
+++ aliases.nix
@@ -1,7 +1,7 @@
 { pkgs, misc, ... }: {
   home.shellAliases = {
     "apply-macbook" = "nix run --impure home-manager/master -- -b bak switch --flake .#ianks@macbook";
-    "gpsh" = "git push";
+    "gpsh" = "git push -u origin \"$(git rev-parse --abbrev-ref HEAD)\"";
     "be" = "bundle exec";
     "gri" = "git rebase -i";
     "v" = "nvim";
