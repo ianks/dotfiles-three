639370dbac8cdf0654cb2f6d5c186fed56c46fd8
Ian Ker-Seymer <hello@ianks.com>
2024-02-01 01:57:52 -0500

    More stuff

--- programs.nix
+++ programs.nix
@@ -11,6 +11,14 @@
   };
   programs.gpg.enable = true;
   programs.zsh.enable = true;
+  programs.ripgrep.enable = true;
+  programs.fd.enable = true;
+  programs.bat.enable = true;
+  programs.exa.enable = true;
+  programs.gh.enable = true;
+  programs.gh.settings = {
+    enableNotifications = false;
+  };
   programs.git = {
     enable = true;
     aliases = {
