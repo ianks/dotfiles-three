24ff1cc059c2ebc877dcf32223931fb52786eef8
Ian Ker-Seymer <hello@ianks.com>
2024-02-03 21:58:37 -0500

    Add gc alias

--- aliases.nix
+++ aliases.nix
@@ -1,11 +1,11 @@
 { pkgs, misc, ... }: {
   home.shellAliases = {
-    "apply-macbook" = "nix run --impure home-manager/master -- -b bak switch --flake .#ianks@macbook";
-    "gpsh" = "git push -u origin \"$(git rev-parse --abbrev-ref HEAD)\"";
+    "gpsh" = ''git push -u origin "$(git rev-parse --abbrev-ref HEAD)"'';
     "be" = "bundle exec";
     "gri" = "git rebase -i";
     "v" = "nvim";
     "df" = "df -h";
-    "gch" = "git checkout \"$(git recent-branches | fzf | tr -d '[:space:]')\"";
+    "gch" = ''git checkout "$(git recent-branches | fzf | tr -d '[:space:]')"'';
+    "gc" = "git commit";
   };
 }
